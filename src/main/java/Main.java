1c3e375b46caad2348f02caf06ef7684b15b05db
vicenteedu <vicentelemesens@gmail.com>
2026-05-01 16:32:04 -0300

    adicionando Alteração e Inclusão

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
 import javax.swing.JOptionPane;
 
-public class Main {
+public class Main { 
+    
+    static String[] nomes = new String[100];
+    static double[] precos = new double[100];
+    static int[] quantidades = new int[100];
+    static String[] unidades = new String[100];
+    static int total = 0;
 
     public static void main(String[] args) {
 
         char menuCHAR;
 
-        // MENU PRINCIPAL COM LOOP
+        // MENU PRINCIPAL 
         do {
             String menu = JOptionPane.showInputDialog("""
                                     XYZ COMERCIO DE PRODUTOS LTDA.
@@ -68,7 +74,7 @@ public class Main {
                     incluir();
                     break;
                 case '2':
-                    JOptionPane.showMessageDialog(null, "Alteração ainda não implementada.");
+                    alterar();
                     break;
                 case '3':
                     JOptionPane.showMessageDialog(null, "Consulta ainda não implementada.");
@@ -86,9 +92,11 @@ public class Main {
         char newinclusaoCHAR;
 
         do {
-            String nome = JOptionPane.showInputDialog("Informe o nome do produto:");
-            double preco = Double.parseDouble(JOptionPane.showInputDialog("Informe o preço:"));
-            int qtde = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade:"));
+            String nome = JOptionPane.showInputDialog("Informe o nome do produto: ");
+            double preco = Double.parseDouble(JOptionPane.showInputDialog("Informe o preço: "));
+            String Uni = JOptionPane.showInputDialog("Informe a unidade de medida (ex: Kg,Un,Cx): ");
+            int qtde = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade: "));
+            
 
             String confirma = JOptionPane.showInputDialog("""
                     Confirma a inclusão?
@@ -99,6 +107,12 @@ public class Main {
             char confirmaCHAR = confirma.charAt(0);
 
             if (confirmaCHAR == 's' || confirmaCHAR == 'S') {
+                nomes[total] = nome;
+                precos[total] = preco;
+                unidades[total] = Uni;
+                quantidades[total] = qtde;
+                total++;
+                
                 JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
             } else {
                 JOptionPane.showMessageDialog(null, "Inclusão cancelada!");
@@ -114,7 +128,65 @@ public class Main {
 
         } while (newinclusaoCHAR == 's' || newinclusaoCHAR == 'S');
     }
+    
+    // ================= altereção ================= 
+    static void alterar(){
+    char newalteracaoChar;
+    
+    do{
+        String nomeBusca = JOptionPane.showInputDialog("Informe o nome do produto que deseja alterar:");
+            int pos = -1;
+            
+            for (int i = 0; i < total; i++) {
+                if (nomes[i].equalsIgnoreCase(nomeBusca)) {
+                    pos = i;
+                    break;
+                }
+            }
+            if (pos == -1) {
+                JOptionPane.showMessageDialog(null, "Produto não encontrado!");
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "Produto encontrado!\n"
+                        + "Nome: " + nomes[pos]
+                        + "\nPreço: " + precos[pos]
+                        + "\nQuantidade: " + quantidades[pos] + unidades[pos]);
+                
+                double novoPreco = Double.parseDouble(JOptionPane.showInputDialog("Novo preço:"));
+                int novaQtd = Integer.parseInt(JOptionPane.showInputDialog("Nova quantidade:"));
+                String novaUni = JOptionPane.showInputDialog("Nova unidade de medida (ex: Kg,Un,Cx):");
 
+                String confirma = JOptionPane.showInputDialog("""
+                    Confirma alteração?
+                    S - Sim
+                    N - Não
+                    """);
+
+                char confirmaChar = confirma.charAt(0);
+
+                if (confirmaChar == 'S' || confirmaChar == 's') {
+                    precos[pos] = novoPreco;
+                    unidades[pos] = novaUni;
+                    quantidades[pos] = novaQtd;
+
+                    JOptionPane.showMessageDialog(null, "Produto alterado com sucesso!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Alteração cancelada!");
+                }
+            }
+
+            
+            String nova = JOptionPane.showInputDialog("""
+                Deseja alterar outro produto?
+                S - Sim
+                N - Não
+                """);
+
+            newalteracaoChar = nova.charAt(0);
+
+        } while (newalteracaoChar == 'S' || newalteracaoChar == 's');
+    }
+    
     // ================= MOVIMENTAÇÃO =================
     static void menuMovimentacao() {
         char op;
